c1d3773a103ff014e02636363cb19739db3eeec7
TXyy117 <2973923225@qq.com>
2025-03-23 21:05:46 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,14 @@ import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 
-// The following are known larger packages or packages that can be loaded asynchronously.
 const individuallyPackages = ['activities', 'github.svg', 'grid.svg'];
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',  // 监听所有网络接口，允许外部访问
+    port: 5173,       // 指定端口
+    strictPort: true, // 如果端口被占用会直接报错，而不是切换到其他端口
+  },
   plugins: [
     react(),
     viteTsconfigPaths(),
@@ -41,16 +44,12 @@ export default defineConfig({
   },
   build: {
     manifest: true,
-    outDir: './dist', // for user easy to use, vercel use default dir -> dist
+    outDir: './dist',
     rollupOptions: {
       output: {
-        manualChunks: (id: string) => {
+        manualChunks: (id) => {
           if (id.includes('node_modules')) {
             return 'vendors';
-            // If there will be more and more external packages referenced in the future,
-            // the following approach can be considered.
-            // const name = id.split('node_modules/')[1].split('/');
-            // return name[0] == '.pnpm' ? name[1] : name[0];
           } else {
             for (const item of individuallyPackages) {
               if (id.includes(item)) {
